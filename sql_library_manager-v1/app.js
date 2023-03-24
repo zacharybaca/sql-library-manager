867af1e39643161b20c931feca4f6eacd57ff6bb
Zachary Baca <bacazach@icloud.com>
2023-03-23 22:45:05 -0500

    refactored some files

--- sql_library_manager-v1/app.js
+++ sql_library_manager-v1/app.js
@@ -3,12 +3,9 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-const Sequelize = require("sequelize");
+const { sequelize } = require("./models/index.js");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "library.db",
-});
+/*Next Step: Create page-not-found template to use Error Page Endpoint*/
 
 (async () => {
   await sequelize.sync({
@@ -39,21 +36,26 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+// 404 Error Handler
+app.use((req, res, next) => {
+  const err = new Error("Error 404");
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+// Global Error Handler
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res
+      .status(404)
+      .render("page-not-found", { err: err.message, status: err.status });
+  } else {
+    err.message =
+      err.message || "A server error has occurred, please try again later.";
+    res
+      .status(err.status || 500)
+      .render("error", { err: err.message, status: err.status });
+  }
 });
 
 module.exports = app;
